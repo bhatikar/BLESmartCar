c39005a1f26781f27d6d00719a0258f7da07200a
Abhijeet Bhatikar <Abhijeet Bhatikar>
2019-09-15 00:36:53 +0100

    Updated code

--- code/Dable/blecar/blecar.ino
+++ code/Dable/blecar/blecar.ino
@@ -1,12 +1,12 @@
 // Motor A
-int enable1Pin = 4; 
-int motor1Pin1 = 13; 
-int motor1Pin2 = 27; 
+int enable1Pin = 4;
+int motor1Pin1 = 13;
+int motor1Pin2 = 27;
 
 // Motor B
-int enable2Pin = 32; 
-int motor2Pin1 = 33; 
-int motor2Pin2 = 25; 
+int enable2Pin = 32;
+int motor2Pin1 = 33;
+int motor2Pin2 = 25;
 
 // Setting PWM properties
 const int freq = 30000;
@@ -74,9 +74,9 @@ void moveBackward()
    // Move DC motor backwards at maximum speed
   Serial.println("Moving Backwards");
   digitalWrite(motor1Pin1, HIGH);
-  digitalWrite(motor1Pin2, LOW); 
+  digitalWrite(motor1Pin2, LOW);
   digitalWrite(motor2Pin1, HIGH);
-  digitalWrite(motor2Pin2, LOW); 
+  digitalWrite(motor2Pin2, LOW);
   ledcWrite(pwmChannel1, 110);
   ledcWrite(pwmChannel2, 240 + correctionFactor);
   delay(400);
@@ -98,9 +98,9 @@ void turnRight()
    // Move DC motor backwards at maximum speed
   Serial.println("Moving Right");
   digitalWrite(motor1Pin1, HIGH);
-  digitalWrite(motor1Pin2, LOW); 
+  digitalWrite(motor1Pin2, LOW);
   digitalWrite(motor2Pin1, LOW);
-  digitalWrite(motor2Pin2, HIGH); 
+  digitalWrite(motor2Pin2, HIGH);
   ledcWrite(pwmChannel1, 190);
   //ledcWrite(pwmChannel2, 190 + correctionFactor);
   delay(200);
@@ -111,9 +111,9 @@ void turnLeft()
    // Move DC motor backwards at maximum speed
   Serial.println("Moving Left");
   digitalWrite(motor1Pin1, LOW);
-  digitalWrite(motor1Pin2, HIGH); 
+  digitalWrite(motor1Pin2, HIGH);
   digitalWrite(motor2Pin1, HIGH);
-  digitalWrite(motor2Pin2, LOW); 
+  digitalWrite(motor2Pin2, LOW);
   //ledcWrite(pwmChannel1, 190);
   ledcWrite(pwmChannel2, 190 + correctionFactor);
   delay(200);
@@ -129,7 +129,6 @@ int lookLeft()
   return distance;
 }
 
-
 int lookRight()
 {
   myservo.write(60);
@@ -140,61 +139,20 @@ int lookRight()
   return distance;
 }
 
-void autonomousCar() {
-
- int distanceR = 0;
- int distanceL =  0;
- //delay(40);
- 
- if(distance <= 50)
- {
-  moveStop();
-  delay(100);
-  moveBackward();
-  delay(300);
-  moveStop();
-  delay(200);
-  distanceR = lookRight();
-  delay(200);
-  distanceL = lookLeft();
-  delay(200);
-
-  if(distanceR >= distanceL)
-  {
-    turnRight();
-    moveStop();
-  } 
-  else {
-    turnLeft();
-    moveStop();
-  }
- }
- else {
-  moveForward();
- }
- distance = distanceSensor.measureDistanceCm();
- if(distance <= 50)
- {
-  moveStop();
-  delay(100);
- }
- Serial.print(distance);
-}
-
 void setup_motorshield()
 {
   pinMode(motor1Pin1, OUTPUT);
   pinMode(motor1Pin2, OUTPUT);
   pinMode(enable1Pin, OUTPUT);
-  
+
   pinMode(motor2Pin1, OUTPUT);
   pinMode(motor2Pin2, OUTPUT);
   pinMode(enable2Pin, OUTPUT);
-  
+
   // configure LED PWM functionalitites
   ledcSetup(pwmChannel1, freq, resolution);
   ledcSetup(pwmChannel2, freq, resolution);
-  
+
   // attach the channel to the GPIO to be controlled
   ledcAttachPin(enable1Pin, pwmChannel1);
   ledcAttachPin(enable2Pin, pwmChannel2);
@@ -203,8 +161,8 @@ void setup_motorshield()
 void setup_servo()
 {
   myservo.attach(
-        servoPin, 
-        Servo::CHANNEL_NOT_ATTACHED, 
+        servoPin,
+        Servo::CHANNEL_NOT_ATTACHED,
         30,
         150
     );
@@ -222,10 +180,55 @@ void setup() {
   Dabble.begin("BLE Smart Car");
 }
 
-void loop() {
+void autonomousCar() {
 
-  if(autonomous)
-    autonomousCar();
+ int distanceR = 0;
+ int distanceL =  0;
+ //delay(40);
+
+ Dabble.processInput();
+ if (GamePad.isCrossPressed())
+ {
+     autonomous = false;
+     remoteStop();
+ }
+
+ if(distance <= 45)
+ {
+  moveStop();
+  delay(100);
+  moveBackward();
+  delay(300);
+  moveStop();
+  delay(200);
+  distanceR = lookRight();
+  delay(200);
+  distanceL = lookLeft();
+  delay(200);
+
+  if(distanceR >= distanceL)
+  {
+    turnRight();
+    moveStop();
+  }
+  else {
+    turnLeft();
+    moveStop();
+  }
+ }
+ else {
+  moveForward();
+ }
+ distance = distanceSensor.measureDistanceCm();
+ if(distance <= 45)
+ {
+  moveStop();
+  delay(100);
+ }
+ Serial.print(distance);
+}
+
+void loop() {
 
   Dabble.processInput();
 
@@ -264,4 +267,7 @@ void loop() {
      autonomous = false;
      remoteStop();
   }
+
+  if(autonomous)
+    autonomousCar();
 }
